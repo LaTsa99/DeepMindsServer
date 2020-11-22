6f3fa4e8d4fb469af3e366f895eb243ab3ba28ab
LaTsa99 <szapula.laszlo.99@gmail.com>
2020-11-22 17:59:34 +0100

    Locally working, maybe now...

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from flask import Flask, request, jsonify, render_template, flash, redirect
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from werkzeug.utils import secure_filename
+import numpy as np
 
 UPLOAD_FOLDER = '.'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-gdown.cached_download('https://drive.google.com/file/d/1lJ9tPyfO2IJ0OwlfSUEnqq-3l2KfSd_F/view?usp=sharing', 'model.hdf5')
+gdown.cached_download('https://drive.google.com/u/0/uc?export=download&confirm=Moxk&id=1lJ9tPyfO2IJ0OwlfSUEnqq-3l2KfSd_F', 'model.hdf5')
 model = load_model('model.hdf5')
 
 def allowed_file(filename):
@@ -26,7 +27,7 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
 	filename = ''
-	imagefile = flask.request.files['image']
+	imagefile = request.files['image']
 	if imagefile.filename == '':
 		flash('No selected file')
 		return redirect(request.url)
@@ -36,15 +37,16 @@ def predict():
 	
 	input_img = image.load_img(os.path.join(app.config['UPLOAD_FOLDER'], filename), target_size=(299, 299))
 	x = image.img_to_array(input_img)
+	x = x / 255
 	x = np.expand_dims(x, axis=0)
-	x = preprocess_input(x)
 	pred = model.predict(x)
 	os.remove(filename)
+	print(pred)
 	if pred > 0.5:
-		return 1
+		return "NotBikini"
 	else:
-		return 0
+		return "Bikini"
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
 	app.run()
